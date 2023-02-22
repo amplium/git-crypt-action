7ff6cbdca1adec8fb16af7089ea2991cf357b7ef
scheatkode <scheatkode@gmail.com>
2023-02-22 22:50:09 +0100

    Refactor main script

--- main.sh
+++ main.sh
@@ -3,20 +3,22 @@
 set -e
 set -u
 
+git config --global --add safe.directory "${PWD}"
+
 if [ "${INPUT_KEY:+x}" = 'x' ]; then
 	if [ "${INPUT_KEY_ENCODED:+x}" = 'x' ]; then
 		echo 'Either "key" or "key_encoded" should be specified, not both'
 		exit 1
 	fi
 
-	key="${INPUT_KEY}"
-elif [ "${INPUT_KEY_ENCODED:+x}" = 'x' ]; then
-	key="$(echo "${INPUT_KEY_ENCODED}" | base64 -d)"
-else
-	echo 'Neither "key" or "key_encoded" specified, aborting'
-	exit 1
+	echo "${INPUT_KEY}" | git-crypt unlock -
+	exit 0
 fi
 
-git config --global --add safe.directory "${PWD}"
+if [ "${INPUT_KEY_ENCODED:+x}" = 'x' ]; then
+	echo "${INPUT_KEY_ENCODED}" | base64 -d | git-crypt unlock -
+	exit 0
+fi
 
-echo "${key}" | git-crypt unlock -
+echo 'Neither "key" or "key_encoded" specified, aborting'
+exit 1
